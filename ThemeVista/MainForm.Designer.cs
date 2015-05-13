9a2332bd414542136e8bd2afde82ac5d8bcd0362
farjadbullet <farjad_bullet@rocketmail.com>
2015-05-13 11:32:52 +0500

    Maybe Got the solution...

--- ThemeVista/MainForm.Designer.cs
+++ ThemeVista/MainForm.Designer.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components;
+        private System.ComponentModel.IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.visualStudio2012DarkTheme1 = new Telerik.WinControls.Themes.VisualStudio2012DarkTheme();
+            this.FileUpload = new Telerik.WinControls.UI.RadBrowseEditor();
+            this.BtnGenerate = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.FileUpload)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnGenerate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FileUpload
+            // 
+            this.FileUpload.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FileUpload.Location = new System.Drawing.Point(12, 12);
+            this.FileUpload.Name = "FileUpload";
+            this.FileUpload.Size = new System.Drawing.Size(185, 28);
+            this.FileUpload.TabIndex = 0;
+            this.FileUpload.Text = "FileUpload";
+            this.FileUpload.ThemeName = "VisualStudio2012Dark";
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(218, 12);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(138, 28);
+            this.BtnGenerate.TabIndex = 1;
+            this.BtnGenerate.Text = "radButton1";
+            this.BtnGenerate.ThemeName = "VisualStudio2012Dark";
+            this.BtnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(650, 447);
+            this.Controls.Add(this.BtnGenerate);
+            this.Controls.Add(this.FileUpload);
+            this.Name = "MainForm";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "MainForm";
+            this.ThemeName = "VisualStudio2012Dark";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FileUpload)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnGenerate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Telerik.WinControls.Themes.VisualStudio2012DarkTheme visualStudio2012DarkTheme1;
+        private Telerik.WinControls.UI.RadBrowseEditor FileUpload;
+        private Telerik.WinControls.UI.RadButton BtnGenerate;
     }
 }
-
--- ThemeVista/MainForm.cs
+++ ThemeVista/MainForm.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using Telerik.WinControls;
+using DataTransformer;
 namespace ThemeVista
 {
-    public partial class MainForm : Form
+    public partial class MainForm : Telerik.WinControls.UI.RadForm
     {
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            var x = this.FileUpload.Value;
+            DataTranalyzer dt = new DataTranalyzer();
+            dt.OpenFile(x);
         }
     }
 }
